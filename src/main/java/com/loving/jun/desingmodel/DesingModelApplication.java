1d2fcb77ab8cf51f70eace0d054f785c3b287bbd
yijunjun <25388011977@qq.com>
2022-04-07 22:09:30 +0800

    Initial commit

---
+++ src/main/java/com/loving/jun/desingmodel/DesingModelApplication.java
@@ -0,0 +1,13 @@
+package com.loving.jun.desingmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesingModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesingModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loving/jun/desingmodel/DesingModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loving.jun.desingmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesingModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
