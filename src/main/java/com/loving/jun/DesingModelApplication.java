6e079c73db972ce460406904ac00f4a6f6b73588
yijunjun <25388011977@qq.com>
2022-04-11 20:59:24 +0800

    目录结构变更

--- src/main/java/com/loving/jun/DesingModelApplication.java
+++ src/main/java/com/loving/jun/DesingModelApplication.java
@@ -1,4 +1,4 @@
-package com.loving.jun.desingmodel;
+package com.loving.jun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
